a2a76fb8ea1ec6c4827d28787e873dcb5ed31b65
facuricher <facu.richer@gmail.com>
2022-01-07 19:55:21 -0300

    componente avatar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import { Avatar } from './components/Avatar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
-      </header>
+      <main className="App-main">
+          <Avatar userName="Roman Alberto" />
+      </main>
     </div>
   );
 }
