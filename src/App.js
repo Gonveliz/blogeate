02756791be80c2a2f4ece4417f805ffbd02ed4af
facuricher <facu.richer@gmail.com>
2022-01-10 19:17:50 -0300

    agregando navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import { Avatar } from './components/Avatar';
+import Navbar from './components/Navbar';
+
 
 function App() {
   return (
     <div className="App">
       <main className="App-main">
+          <Navbar/>
           <Avatar userName="Roman Alberto" />
       </main>
     </div>
